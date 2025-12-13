d67bf068fb9804e69ab0ea92f0eb1318a5a3a503
SIMO Yann <simoyann2004@gmail.com>
2025-12-13 06:12:40 +0100

    Auto-generated commit

--- chunk-6WXPQQDQ.js
+++
@@ -1,1 +0,0 @@
-var t={production:!1,apiEntry:"http://biometric-staff-attendance-mgt-production.up.railway.app:8080",apiUrl:"http://biometric-staff-attendance-mgt-production.up.railway.app:8080/api"};export{t as a};
